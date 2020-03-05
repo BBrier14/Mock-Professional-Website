58f9560bc859b24b4cf5c6f3cccccb72906d6c6d
Benjamin Brier <ben.brier14@gmail.com>
2020-03-05 16:08:42 -0500

    picture animation on main page works

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 function init() {
 	const slides = document.querySelectorAll('.slide');
-	const pages = document.querySelector('.page');
+	const pages = document.querySelectorAll('.page');
 	const backgrounds = [
 		`radial-gradient(#2B3760, #0B1023)`,
 		`radial-gradient(#4E3022, #161616)`,
 		`radial-gradient(#4EE4342, #161616)`
 	];
+	//Tracker
+	let current = 0;
 
 	slides.forEach((slide, index) => {
 		slide.addEventListener('click', function() {
 			changeDots(this);
+			nextSlide(index);
 		});
 	});
 
@@ -18,6 +21,23 @@ function init() {
 			slide.classList.remove('active');
 		});
 		dot.classList.add('active');
+	}
+	function nextSlide(pageNumber) {
+		const nextPage = pages[pageNumber];
+		const currentPage = pages[current];
+		const nextLeft = nextPage.querySelector('.hero .model-left');
+		const nextRight = nextPage.querySelector('.hero .model-right');
+		const currentLeft = currentPage.querySelector('.hero .model-left');
+		const currentRight = currentPage.querySelector('.hero .model-right');
+		const nextText = nextPage.querySelector('.details');
+		const portfolio = document.querySelector('.portfolio');
+
+		const t1 = new TimelineMax();
+
+		t1
+			.fromTo(currentLeft, 0.3, { y: '-10%' }, { y: '-100%' })
+			.fromTo(currentRight, 0.3, { y: '10%' }, { y: '-100%' }, '-=0.2')
+			.to(portfolio, 0.3, { backgroundImage: backgrounds[pageNumber] });
 	}
 }
 
